d859b0673decb6301997280321d3c2925d4e9073
Rajat Kaushik <M1046872@mindtree.com>
2020-03-23 17:35:15 +0530

    q

---
+++ precache-manifest.2725bf8880cd64d8fb73d9c7e3650c77.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b588ce0a15b644a182ba2d67d38c8872",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2c5c3b60dac0e8a743c",
+    "url": "/static/css/2.2727ec5b.chunk.css"
+  },
+  {
+    "revision": "741d09222c9f6838496d",
+    "url": "/static/css/main.bc3eceb0.chunk.css"
+  },
+  {
+    "revision": "c2c5c3b60dac0e8a743c",
+    "url": "/static/js/2.3aa4b47b.chunk.js"
+  },
+  {
+    "revision": "67c03f7e2d774111b9921a943c6aa3a5",
+    "url": "/static/js/2.3aa4b47b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "741d09222c9f6838496d",
+    "url": "/static/js/main.ecbb9280.chunk.js"
+  },
+  {
+    "revision": "6896bc3088e80b2ecd0f",
+    "url": "/static/js/runtime-main.28a6a14a.js"
+  },
+  {
+    "revision": "cc26d69a6f8204df8725651bde87c3fb",
+    "url": "/static/media/1.cc26d69a.png"
+  },
+  {
+    "revision": "71cccb451b7aa33b46601bf3e3c0a0ee",
+    "url": "/static/media/2.71cccb45.png"
+  },
+  {
+    "revision": "e5c1c9ff3438d05bb0bb7a2108ebbfe0",
+    "url": "/static/media/3.e5c1c9ff.png"
+  },
+  {
+    "revision": "4596c2a8b2f262541b6d75b1a66e6466",
+    "url": "/static/media/4.4596c2a8.png"
+  },
+  {
+    "revision": "4ce460d54cdf7946687c1bd37959a605",
+    "url": "/static/media/art.4ce460d5.jpg"
+  },
+  {
+    "revision": "b0bcb1c66353ec7692a1a6bb7cc3c481",
+    "url": "/static/media/banner-1.b0bcb1c6.jpg"
+  },
+  {
+    "revision": "0ce5a274de27bebfb65bbce47527c85b",
+    "url": "/static/media/banner-2.0ce5a274.jpg"
+  },
+  {
+    "revision": "f199ce3fce3d4ee8252f1b90aa013393",
+    "url": "/static/media/banner-3.f199ce3f.jpg"
+  },
+  {
+    "revision": "4dfa259e5372f976a3efe673c31c368e",
+    "url": "/static/media/blog.4dfa259e.jpg"
+  },
+  {
+    "revision": "0d45a0bfe31743faa26d17a3e6040de5",
+    "url": "/static/media/book-1.0d45a0bf.jpg"
+  },
+  {
+    "revision": "52d67d9a1605931093bab5862fe73601",
+    "url": "/static/media/book-2.52d67d9a.jpg"
+  },
+  {
+    "revision": "0ef1d5abfafa85e3034b70606142c8a3",
+    "url": "/static/media/book-3.0ef1d5ab.jpg"
+  },
+  {
+    "revision": "c2cdc6c36df860d271c92543067feb09",
+    "url": "/static/media/book-4.c2cdc6c3.jpg"
+  },
+  {
+    "revision": "aeaa2e89f24c66133090aeea20170c8f",
+    "url": "/static/media/book-5.aeaa2e89.jpg"
+  },
+  {
+    "revision": "c6536ab808d3bf1afebcb758b3bcce79",
+    "url": "/static/media/fiction.c6536ab8.jpg"
+  },
+  {
+    "revision": "47056e3ef7530906a676568e6e41b7a8",
+    "url": "/static/media/history.47056e3e.jpg"
+  },
+  {
+    "revision": "2fd3e87c0924fbdf8896f82e80fd5108",
+    "url": "/static/media/profile-pic.2fd3e87c.svg"
+  },
+  {
+    "revision": "31b47dd6477d4240b050b18986de283a",
+    "url": "/static/media/scifi.31b47dd6.jpg"
+  },
+  {
+    "revision": "b17c30d7407f91d2df9a40b5261ae8ca",
+    "url": "/static/media/user.b17c30d7.png"
+  }
+]);
